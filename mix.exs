d7f81b48ce4edbb351c1bcf1292c48ee6171b3a4
Namdak Tonpa <maxim@synrc.com>
2019-10-27 01:59:38 +0300

    cname

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule RT.Mixfile do
+  use Mix.Project
+
+  def project() do
+    [
+      app: :rt,
+      version: "0.10.0",
+      elixir: "~> 1.7",
+      description: "RT Erlang Runtime",
+      package: package(),
+      deps: deps()
+    ]
+  end
+
+  def package do
+    [
+      files: ~w(doc src mix.exs LICENSE),
+      licenses: ["ISC"],
+      maintainers: ["Namdak Tonpa"],
+      name: :rt,
+      links: %{"GitHub" => "https://github.com/synrc/rt"}
+    ]
+  end
+
+  def application() do
+    [ mod: {:rt, []} ]
+  end
+
+  def deps() do
+    [
+      {:ex_doc, "~> 0.11", only: :dev}
+    ]
+  end
+end
